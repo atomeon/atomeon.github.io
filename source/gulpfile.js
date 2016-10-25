b64e48b8adbce50b4125693002849dd027fe20a2
Timur A <atomeon@gmail.com>
2016-10-25 15:47:09 +0600

    browser-sync working

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -7,6 +7,7 @@
     const sourcemaps = require('gulp-sourcemaps')
     const debug = require('gulp-debug')
     const del = require('del');
+    var browserSync = require('browser-sync').create();
 
 
     gulp.task('sass', function() {
@@ -20,8 +21,32 @@
             .pipe(gulp.dest('css'));
     });
 
+    gulp.task('sass-reload', ['sass'], function (done) {
+      browserSync.reload();
+      done();
+    });
+
     gulp.task('clean', function(){
       return del('css');
+    });
+
+    gulp.task('browser-sync', function() {
+    browserSync.init({
+          server: {
+              baseDir: "../"
+          }
+        });
+    });
+
+ 
+    gulp.task('serve', ['clean','sass'], function() {
+
+        browserSync.init({
+            server: "../"
+        });
+
+        gulp.watch(['scss/**/*.scss'], ['clean','sass']).on('change', browserSync.reload);;
+        gulp.watch("../*.html").on('change', browserSync.reload);
     });
 
     // gulp.task('build', gulp.series('clean', 'sass'));
@@ -29,6 +54,8 @@
 
     // gulp.watch('scss/app.scss', ['clean','sass']);
 
-    gulp.task('default', ['clean','sass'], function() {
-      gulp.watch(['scss/**/*.scss'], ['clean','sass']);
-  });
+    // gulp.task('default', ['clean','sass'], function() {
+    //     gulp.watch(['scss/**/*.scss'], ['clean','sass']);
+    // });
+
+    gulp.task('default', ['serve']);
